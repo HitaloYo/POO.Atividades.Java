526d9bfaca410f250bd226b489c9f62b8a0b87b2
HitaloYo <76579977+HitaloYo@users.noreply.github.com>
2024-04-10 03:29:29 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Figura minhaFigura = new Figura();
+        minhaFigura.setCor("azul");
+        System.out.println("A cor da figura é: " + minhaFigura.getCor()); // Saída: A cor da figura é: azul
+    }
+}
