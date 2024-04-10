a7d4866e3f17fd40708a1449f3882e20ca9fffd3
HitaloYo <76579977+HitaloYo@users.noreply.github.com>
2024-04-10 04:10:23 -0300

    Delete Atividade Abstract/Main.java

--- Atividade Abstract/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Figura minhaFigura = new Figura();
-        minhaFigura.setCor("azul");
-        System.out.println("A cor da figura é: " + minhaFigura.getCor()); // Saída: A cor da figura é: azul
-    }
-}
