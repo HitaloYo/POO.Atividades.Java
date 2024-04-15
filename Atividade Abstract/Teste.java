50f98c8c7a7696564ad8cc1637c9b1900b31a680
HitaloYo <76579977+HitaloYo@users.noreply.github.com>
2024-04-15 20:13:37 -0300

    Update Teste.java

--- Atividade Abstract/Teste.java
+++ Atividade Abstract/Teste.java
@@ -3,12 +3,15 @@ public class Teste {
         Retangulo retangulo1 = new Retangulo(10,15,"Azul");
         Triangulo triangulo1 = new Triangulo(25, 16, "Verde");
         Circulo circulo1 = new Circulo(25, "preto");
-        //Quadrado quadrado = new quadrado(PARAMENTROS);
+        Quadrado quadrado1 = new Quadrado(28, 28, "Cinza");
+        
+        
         System.out.println(retangulo1.toString());
         System.out.println("==========");
         System.out.println(triangulo1.toString());
         System.out.println("==========");
         System.out.println(circulo1.toString());
         System.out.println("==========");
+        System.out.println(quadrado1.toString());
     }
 }
