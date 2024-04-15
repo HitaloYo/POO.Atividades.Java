8a5ab0b7729a0b0377eadc8a994fabfcc7489f88
HitaloYo <76579977+HitaloYo@users.noreply.github.com>
2024-04-15 20:12:54 -0300

    Update Quadrado.java

--- Atividade Abstract/Quadrado.java
+++ Atividade Abstract/Quadrado.java
@@ -1,3 +1,14 @@
-public class Quadrado {
-    //Não sei fazer Herança multipla no java :D
+public class Quadrado extends Retangulo {
+    
+    public Quadrado (double lado1, double lado2, String cor){
+        super(lado1, lado2, cor);
+    }
+
+    public String toString(){
+        String dados = "A Base do Quadrado é: " + this.lado1;
+        dados += "\nA altura do Quadrado é: " + this.lado2;
+        dados += "\nA area do Quadrado é: " + area() ;
+        return dados;
+    }
+    
 }
